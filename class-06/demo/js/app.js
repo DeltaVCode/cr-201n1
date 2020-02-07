53ec62f18330f16ded21a6d07981f7693cab1636
Craig Barkley <craig@iowastartupaccelerator.com>
2020-02-07 13:30:45 -0600

    add comments and clean up

--- class-06/demo/js/app.js
+++ class-06/demo/js/app.js
@@ -1,23 +1,9 @@
 'use strict';
-
 console.log('It is connected....');
-
-
-
 // Problem Domain: the Cedar Rapids Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
 
+//Each Kitten's profile should have:  Name, Breed, gender, random age assigned, a list of what they like, an image, is good with kids, is good with dogs, is good with other animals 
 
-
-//Each Kitten's profile should have: 
-// Name
-// Breed
-// - gender   -- isFixed. 
-// - random age assigned
-// a list of what they like.
-// an image
-// is good with kids
-// is good with dogs
-// is good with other animals 
 //TODO: Dynamically, generate kitten objects using form data.
 
 
@@ -38,10 +24,7 @@ var petOne = {
 };
 petOne.setAge();
 
-
 //console.log('Pets Name ', petOne.name);
-
-
 
 // Add Second Object 
 var petTwo = {
@@ -61,22 +44,21 @@ var petTwo = {
 petTwo.setAge();
 
 
-
 function randomAge(min, max){
     return Math.floor(Math.random() * (max - min +1) + min);
 }
-//Does Gender Matter? 
+
+
 petOne.isFixed = true;
 petTwo.isFixed = true;
 
 
+////////////////// Create DOM elements and render in html///////////////////////////
 
-// Create DOM elements and render in html
-
-//creates a new element the parent element of the child element that we will create below to render in html.
+//create new element, The "Parent"  
 var parentElement = document.getElementById('kittenProfiles');
 
-//inside that element article, h2, p, ul, li, interests, image. 
+//Now creat the child elements; article, h2, p, ul, li, interests, image. 
 
 //Create Article
 var article = document.createElement('article');
@@ -103,8 +85,6 @@ for(var i = 0; i < petOne.interests.length; i++){
     ul.appendChild(li);
 }
 
-
-
 //add image
 var petImage = document.createElement('img');
 //Set the value of an attribute on the specified element.
@@ -113,22 +93,23 @@ petImage.setAttribute('alt', 'Cute picture of ' + petOne.name + ', who is a grey
 article.appendChild(petImage);
 
 
-
+//Add image Two 
 var petImageTwo = document.createElement('img');
 //Set the value of an attribute on the specified element.
 petImageTwo.setAttribute('src', 'images/' + petTwo.imageName + '.jpeg');
 petImageTwo.setAttribute('alt', 'Cute picture of ' + petTwo.name + ', who is a orange tabby ready for fun. You should really adopt him.');
 article.appendChild(petImageTwo);
 
-
+//Create an array with the pet objects;
 var adoptPets = [petOne, petTwo];
 var petParagraph = document.getElementById('adoptPets-holder');
 
+//Loop through the objects to show some information on the page. 
 for(var i = 0; i < adoptPets.length; i++){
-var newParagraph = document.createElement('p');
-//set text content with template literals.
-newParagraph.textContent = `A nice kitten to code with is  ${adoptPets[i].name}  because they are good at ${adoptPets[i].interests} .` ;
-petParagraph.appendChild(newParagraph);
+    var newParagraph = document.createElement('p');
+    //set text content with template literals.
+    newParagraph.textContent = `A nice kitten to code with is  ${adoptPets[i].name}  because they are good at ${adoptPets[i].interests} .` ;
+    petParagraph.appendChild(newParagraph);
 }
 
 
